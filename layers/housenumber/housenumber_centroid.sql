13275c34fae5b2f9f9b45ba1c9dbf9dabbb69948
hejiang <18715770814@163.com>
2020-10-13 14:45:30 +0800

    初始化

---
+++ layers/housenumber/housenumber_centroid.sql
@@ -0,0 +1,100 @@
+DROP TRIGGER IF EXISTS trigger_flag ON osm_housenumber_point;
+DROP TRIGGER IF EXISTS trigger_store ON osm_housenumber_point;
+DROP TRIGGER IF EXISTS trigger_refresh ON housenumber.updates;
+
+CREATE SCHEMA IF NOT EXISTS housenumber;
+
+CREATE TABLE IF NOT EXISTS housenumber.osm_ids
+(
+    osm_id bigint
+);
+
+-- etldoc: osm_housenumber_point -> osm_housenumber_point
+CREATE OR REPLACE FUNCTION convert_housenumber_point(full_update boolean) RETURNS void AS
+$$
+    -- Delete housenumber duplicates
+    DELETE FROM osm_housenumber_point
+    WHERE osm_id IN (
+      SELECT pt.osm_id
+      FROM osm_housenumber_point pt
+      INNER JOIN osm_housenumber_point poly
+      ON (ST_GeometryType(poly.geometry) <> 'ST_Point'
+          AND ST_GeometryType(pt.geometry) = 'ST_Point'
+          AND pt.geometry && poly.geometry
+          AND pt.housenumber = poly.housenumber
+          AND (full_update OR pt.osm_id IN (SELECT osm_id FROM housenumber.osm_ids))
+      )
+    );
+
+    UPDATE osm_housenumber_point
+    SET geometry =
+            CASE
+                WHEN ST_NPoints(ST_ConvexHull(geometry)) = ST_NPoints(geometry)
+                    THEN ST_Centroid(geometry)
+                ELSE ST_PointOnSurface(geometry)
+                END
+    WHERE (full_update OR osm_id IN (SELECT osm_id FROM housenumber.osm_ids))
+        AND ST_GeometryType(geometry) <> 'ST_Point';
+$$ LANGUAGE SQL;
+
+SELECT convert_housenumber_point(true);
+
+-- Handle updates
+
+CREATE OR REPLACE FUNCTION housenumber.store() RETURNS trigger AS
+$$
+BEGIN
+    IF (tg_op = 'DELETE') THEN
+        INSERT INTO housenumber.osm_ids VALUES (OLD.osm_id);
+    ELSE
+        INSERT INTO housenumber.osm_ids VALUES (NEW.osm_id);
+    END IF;
+    RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TABLE IF NOT EXISTS housenumber.updates
+(
+    id serial PRIMARY KEY,
+    t text,
+    UNIQUE (t)
+);
+CREATE OR REPLACE FUNCTION housenumber.flag() RETURNS trigger AS
+$$
+BEGIN
+    INSERT INTO housenumber.updates(t) VALUES ('y') ON CONFLICT(t) DO NOTHING;
+    RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION housenumber.refresh() RETURNS trigger AS
+$$
+BEGIN
+    RAISE LOG 'Refresh housenumber';
+    PERFORM convert_housenumber_point(false);
+    -- noinspection SqlWithoutWhere
+    DELETE FROM housenumber.osm_ids;
+    -- noinspection SqlWithoutWhere
+    DELETE FROM housenumber.updates;
+    RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_store
+    AFTER INSERT OR UPDATE OR DELETE
+    ON osm_housenumber_point
+    FOR EACH ROW
+EXECUTE PROCEDURE housenumber.store();
+
+CREATE TRIGGER trigger_flag
+    AFTER INSERT OR UPDATE OR DELETE
+    ON osm_housenumber_point
+    FOR EACH STATEMENT
+EXECUTE PROCEDURE housenumber.flag();
+
+CREATE CONSTRAINT TRIGGER trigger_refresh
+    AFTER INSERT
+    ON housenumber.updates
+    INITIALLY DEFERRED
+    FOR EACH ROW
+EXECUTE PROCEDURE housenumber.refresh();
